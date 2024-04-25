3ce5c4fbb3b866b85ba98b1eb9f6d8f6becf0f06
Scott <scottad638@gmail.com>
2024-04-24 23:07:44 -0400

    Update 5

--- WDL stuff/WDLProject-main/Tests/semisup.py
+++ WDL stuff/WDLProject-main/Tests/semisup.py
@@ -123,7 +123,6 @@ def ssl_assign_all(rho=0.01, init_points=10):
 
 #SSL iteratively assign point after point
 def ssl_solo_act(point_find, rho=0.05, init_points=25):
-    random.seed(10)
     dir_name = 'random_sample_tests/random_samp_32/random_sample_k=32_mu=0.0001_reg=0.08'
     X = torch.load(dir_name + '/coeff.pt').numpy()
     idx = torch.load(dir_name + '/train_index.pt').numpy()
@@ -132,9 +131,8 @@ def ssl_solo_act(point_find, rho=0.05, init_points=25):
     #Needed initialization information
     remap = {0: 0, 1: 1, 10: 2, 11: 3, 12: 4, 13: 5, 14: 6}
     labels = [1, 2, 3, 4, 5, 6]
-
     (gt_data, mask) = helper.gt_and_mask(remap)
-    (all_points, point_grouping, weights) = get_init_points(10, gt_data, labels, idx, X, random=True)
+    (all_points, point_grouping, weights) = get_init_points(init_points, gt_data, labels, idx, X, rand_mark=True)
     dup_point = all_points.copy()
     weight_d_tracker = [np.zeros((num_points, init_points)) for i in range(0, 6)]
     #On scale, spatial-norm dominates, so meant to scale things down
@@ -200,11 +198,7 @@ def ssl_solo_act(point_find, rho=0.05, init_points=25):
             acc +=1
         print('Prediction:', label, 'True label:', gt_data[idx_2][0], marking)
             
-    for i in range(0, len(point_grouping)): 
-        for j in point_grouping[i]: 
-            if (not (j in dup_point)) and (i + 1) == gt_data[j][0]: 
-                acc += 1
-    acc = acc/X.shape[1]
+    acc = acc/point_find
     print(rho, init_points,' TOTAL ACCUARCY: ',  acc)
     coloring = np.zeros(7138)
     for i in range(0, 7138): 
@@ -214,11 +208,12 @@ def ssl_solo_act(point_find, rho=0.05, init_points=25):
         if i in dup_point:
             coloring[i] = 7
 
+    coloring = np.reshape(coloring, (83, 86))
     new_cmap = cmap_init_marker()
     plt.imshow(coloring, cmap=new_cmap)
-    plt.title('Accuracy = ' + str(round(acc, 2)))
-    plt.show()
-    #plt.savefig('ssl_test2/rho=' + str(rho) + '_init_points=' + str(init_points) + '.png')
+    plt.savefig('point_fixer3_rho=' + str(rho) + '_init_points=' + str(init_points) + '_acc=' + 
+                str(round(acc, 2)) +  '.pdf',
+                bbox_inches='tight')
     plt.clf()
 
 #Updates in fancy batch method
@@ -241,7 +236,7 @@ def ssl_batch_error_update(rho=0.01, init_points=1):
     #SCORE UPDATE LOOP
     #UPDATE 2, add only certain points
     acc = 0
-    error = 0.2
+    error = 0.17
     tot_lab = 0
     while (error < 0.21) and (tot_lab != (X.shape[1] - len(dup_point))):
         m_score = np.zeros((7138, 6))
@@ -314,17 +309,16 @@ def ssl_batch_error_update(rho=0.01, init_points=1):
     coloring = np.reshape(coloring, (83, 86))
     new_cmap = cmap_init_marker()
     plt.imshow(coloring, cmap=new_cmap)
-    plt.title('Accuracy = ' + str(round(acc, 2)))
     plt.savefig('ssl_batch_test/rho=' + str(rho) + '_init_points=' + str(init_points) + '.pdf',
                 bbox_inches='tight')
     plt.clf()
 
 
-if __name__ == '__main__':
-
-    rhos = [0.0001, 0.001, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 
-            1]
-    pts = [1, 5, 10, 25, 50, 100]
-    for r in rhos:
-        for p in pts:
-            ssl_batch_error_update(rho=r, init_points=p)
+if __name__ == '__main__':  
+    ssl_solo_act(point_find=1000, rho=0.5, init_points=1)
+    # rhos = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 
+    #         1]
+    # pts = [1, 5, 10, 25, 50, 100]
+    # for r in rhos:
+    #     for p in pts:
+    #         ssl_batch_error_update(rho=r, init_points=p)
